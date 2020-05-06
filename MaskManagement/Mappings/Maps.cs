09fe9d34e5bcb57160751640f78b180ef7eb933f
geomimo <mimoglou22@gmail.com>
2020-05-06 19:37:01 +0300

    Added automapper

---
+++ MaskManagement/Mappings/Maps.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using MaskManagement.Data;
+using MaskManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MaskManagement.Mappings
+{
+    public class Maps : Profile
+    {
+        public Maps()
+        {
+
+            CreateMap<Mask, MaskVM>().ReverseMap();
+            //CreateMap<Customer, CustomerVM>().ReverseMap();
+            CreateMap<Purchase, PurchaseCreateVM>().ReverseMap();
+            CreateMap<Purchase, PurchaseDetailsVM>().ReverseMap();
+            //CreateMap<PurchasedMasks, PurchasedMasksVM>().ReverseMap();
+        }
+    }
+}
